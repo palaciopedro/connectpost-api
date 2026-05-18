e6f131f8921dc429a1426662966e7b99b428934c
palaciopedro <153301312+palaciopedro@users.noreply.github.com>
2026-05-18 15:51:05 -0300

    Referencing user's posts

--- workshopmongo/src/main/java/com/javaprojects/workshopmongo/config/Instantiation.java
+++ workshopmongo/src/main/java/com/javaprojects/workshopmongo/config/Instantiation.java
@@ -40,8 +40,11 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("17/05/2026"), "André Ramalho não dá", "André Ramalho afundou mais um jogo do Corinthians...", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("17/05/2026"), "RESENHA", "CORITIBA 3 X 0 santos", new AuthorDTO(maria));
-
+	
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 
 }
